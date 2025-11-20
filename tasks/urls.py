146ec59aa6d0fffb26f619f78f286db3625f5c38
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-11-20 14:18:15 +0530

    project updated

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import Tasks
 
 urlpatterns = [
-    path('tasks/', Tasks.as_view({'get':'list'}), name='task-list-create'),
-    path('tasks/<int:pk>/', Tasks.as_view({'get':'list'}), name='task-detail'),
+    path('tasks/', Tasks.as_view({'get':'list','post':'create'}), name='task-list-create'),
+    path('tasks/<int:pk>/', Tasks.as_view({'get':'retrieve','put':'update','delete':'destroy'}), name='task-detail'),
 ]
